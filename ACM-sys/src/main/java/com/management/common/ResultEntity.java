0dd88656ac073dea2ff78b0579790a9890b4ccda
Zs89126 <747230633@qq.com>
2023-09-25 16:28:54 +0800

    测试

--- ACM-sys/src/main/java/com/management/common/ResultEntity.java
+++ ACM-sys/src/main/java/com/management/common/ResultEntity.java
@@ -11,7 +11,6 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class ResultEntity {
-
     /**
      * 状态码
      */
--- ACM-sys/src/main/java/com/management/config/MyBatisPlusConfiguration.java
+++ ACM-sys/src/main/java/com/management/config/MyBatisPlusConfiguration.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Configuration;
  */
 @Configuration
 public class MyBatisPlusConfiguration {
+    
 
     @Bean
     public MybatisPlusInterceptor paginationInterceptor() {
